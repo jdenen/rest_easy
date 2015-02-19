9895a437a43ded6c7f8c6e32a650fa086ad70bd9
Johnson Denen <johnson.denen@gmail.com>
2015-02-18 21:57:23 -0500

    make the specs less complicated

--- spec/rest_easy_spec.rb
+++ spec/rest_easy_spec.rb
@@ -2,72 +2,66 @@ require 'rspec'
 require 'rest_easy'
 
 describe RestEasy do
-  describe ".get_until" do
-    context "with passing validation" do
-      it "passes uri and options to RestClient" do
-        expect(RestClient).to receive(:get).with('url', header: 'header').once.and_return(true)
-        RestEasy.get_until('url', 1, header: 'header'){ |r| r }
-      end
-      
-      it "returns true after one iteration" do
-        expect(RestClient).to receive(:get).once.and_return(true)
-        expect(RestEasy.get_until('url'){ |r| r }).to be_truthy
+  let(:uri){ "http://example.com" }
+  
+  describe "#get_until" do
+    it "performs an HTTP GET through the REST client" do
+      expect(RestClient).to receive(:get).with(uri, Hash).and_return(true)
+      RestEasy.get_until(uri){ |resp| resp }
+    end
+    
+    context "when given block resolves to true" do
+      it "returns true" do
+        expect(RestClient).to receive(:get).and_return(true)
+        expect(RestEasy.get_until(uri){ |resp| resp }).to be true
       end
     end
 
-    context "with failing validation" do
-      it "returns false" do
+    context "when given block never resolves to true" do
+      before do
         expect(RestClient).to receive(:get).twice.and_return(false)
-        expect(RestEasy.get_until('url', 1){ |r| r }).to be_falsey
       end
       
-      it "times out after 10 seconds default" do
-        start_time = Time.now
-        expect(RestClient).to receive(:get).exactly(20).times.and_return(false)
-        RestEasy.get_until('url'){ |r| r }
-        expect(Time.now - start_time).to be > 10
+      it "returns false" do
+        expect(RestEasy.get_until(uri, 1){ |resp| resp }).to be false
       end
-      
-      it "times out after n seconds" do
-        start_time = Time.now
-        expect(RestClient).to receive(:get).exactly(6).times.and_return(false)
-        RestEasy.get_until('url', 3){ |r| r }
-        expect(Time.now - start_time).to be_within(1).of(3).and be > 3
+
+      it "waits to timeout" do
+        start = Time.now
+        RestEasy.get_until(uri, 1){ |resp| resp }
+        duration = Time.now - start
+        expect(duration).to be_between 1, 2
       end
     end
   end
 
-  describe ".get_while" do
-    context "with passing validation" do
-      it "passes uri and options to RestClient" do
-        expect(RestClient).to receive(:get).with('url', header: 'header').once.and_return(false)
-        RestEasy.get_while('url', 1, header: 'header'){ |r| r }
+  describe "#get_while" do
+    it "performs an HTTP GET through the REST client" do
+      expect(RestClient).to receive(:get).with(uri, Hash).and_return(false)
+      RestEasy.get_while(uri){ |resp| resp }
+    end
+    
+    context "when given block continues to resolve to true" do
+      before do
+        expect(RestClient).to receive(:get).twice.and_return(true)
       end
       
-      it "returns true after one iteration" do
-        expect(RestClient).to receive(:get).once.and_return(false)
-        expect(RestEasy.get_while('url'){ |r| r }).to be_truthy
+      it "returns false" do
+        expect(RestEasy.get_while(uri, 1){ |resp| resp }).to be false
+      end
+
+      it "waits to timeout" do
+        start = Time.now
+        RestEasy.get_while(uri, 1){ |resp| resp }
+        duration = Time.now - start
+        expect(duration).to be_between 1, 2
       end
     end
 
-    context "with failing validation" do
-      it "returns false" do
-        expect(RestClient).to receive(:get).twice.and_return(true)
-        expect(RestEasy.get_while('url', 1){ |r| r }).to be_falsey
-      end
-
-      it "times out after 10 seconds by default" do
-        start_time = Time.now
-        expect(RestClient).to receive(:get).exactly(20).times.and_return(true)
-        RestEasy.get_while('url'){ |r| r }
-        expect(Time.now - start_time).to be > 10
-      end
-
-      it "times out after n seconds" do
-        start_time = Time.now
-        expect(RestClient).to receive(:get).exactly(6).times.and_return(true)
-        RestEasy.get_while('url', 3){ |r| r }
-        expect(Time.now - start_time).to be_within(1).of(3).and be > 3
+    context "when given block resolves to false" do
+      it "returns true" do
+        expect(RestClient).to receive(:get).and_return(false)
+        expect(RestEasy.get_while(uri){ |resp| resp }).to be true
       end
     end
   end
